dac155a8cfdd7c928e9d34ee98adcfff3fd5da99
AnastasiaDro <adrogunova@gmail.com>
2020-08-11 19:00:43 +0300

    - исправила layout - сделала в фоновом потоке выгрузку слов из списка

--- app/src/main/java/com/drogunova/neuroapps/neuropsy/TenWordsActivity.java
+++ app/src/main/java/com/drogunova/neuroapps/neuropsy/TenWordsActivity.java
@@ -19,6 +19,7 @@ import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 import moxy.MvpAppCompatActivity;
 import moxy.presenter.InjectPresenter;
+import moxy.presenter.ProvidePresenter;
 
 public class TenWordsActivity extends MvpAppCompatActivity implements TenWordsView {
 
@@ -27,46 +28,69 @@ public class TenWordsActivity extends MvpAppCompatActivity implements TenWordsVi
     //массив 10 слов из списка (из строкового ресурса)
     List<String> wordsNamesStringsList;
     private Observable<String> observable;
-    TextView [] wordNamestextViews;
+    TextView [] wordNamesTextViews;
+    private Disposable disposable;
 
     @InjectPresenter
     TenWordsPresener tenWordsPresener;
+
+    @ProvidePresenter
+    TenWordsPresener providePresenter(){
+        TenWordsPresener tenWordsPresener = new TenWordsPresener();
+        tenWordsPresener
+                .setWordsNamesStringsList(Arrays.asList(getResources().getStringArray(R.array.tenWords_firstWordSet_array)));
+        return tenWordsPresener;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_ten_words);
-        TextView [] wordNamestextViews = new TextView[10];
-        findTextViewsToArr(wordNamestextViews);
+        //TextView [] wordNamesTextViews = new TextView[10];
+      //  findTextViewsToArr();
+       // Log.d(TAG, "wordNamesTextViews пуст?? " + wordNamesTextViews.toString());
         //загрузим слова
-        tenWordsPresener.loadWords(this);
+        tenWordsPresener.setNamesToTextViews();
+
     }
 
 
     @Override
     public void setWordsNamesToTextViews(){
-        wordNamestextViews = new TextView[10];
-        findTextViewsToArr(wordNamestextViews);
-        observable = tenWordsPresener.loadWords(this);
-        Disposable disposable = observable.observeOn(AndroidSchedulers.mainThread()).subscribe(s -> {
-            wordNamestextViews[wordsNamesStringsList.indexOf(s)].setText(s);
+        wordNamesTextViews = new TextView[10];
+        findTextViewsToArr();
+        observable = tenWordsPresener.getObs();
+        wordsNamesStringsList = tenWordsPresener.getWordsNamesStringsList();
+        disposable = observable.observeOn(AndroidSchedulers.mainThread()).subscribe(s -> {
+            wordNamesTextViews[wordsNamesStringsList.indexOf(s)].setText(s);
             Log.d(TAG, "setWordsNamesToTextViews in Thread: " + Thread.currentThread().getName());
-        });
+        }, throwable -> {
+            Log.d(TAG, "setWordsNamesToTextViews() onError");
+        }, ()-> {
+            Log.d(TAG, "setWordsNamesToTextViews() onComplete");
+                }
+                );
     }
 
+//    public void subscribe(View view){
+//        disposable = observable.observeOn(AndroidSchedulers.mainThread()).subscribe(s -> {
+//            wordNamestextViews[wordsNamesStringsList.indexOf(s)].setText(s);
+//            Log.d(TAG, "setWordsNamesToTextViews in Thread: " + Thread.currentThread().getName());
+//        });
+//    }
 
-    private void findTextViewsToArr(TextView [] textViews) {
-        textViews[0] = findViewById(R.id.first_word_name_textView);
-        textViews[1] = findViewById(R.id.second_word_name_textView);
-        textViews[2] = findViewById(R.id.third_word_name_textView);
-        textViews[3] = findViewById(R.id.fourth_word_name_textView);
-        textViews[4] = findViewById(R.id.fifth_word_name_textView);
-        textViews[5] = findViewById(R.id.sixth_word_name_textView);
-        textViews[6] = findViewById(R.id.seventh_word_name_textView);
-        textViews[7] = findViewById(R.id.eight_word_name_textView);
-        textViews[8] = findViewById(R.id.nine_word_name_textView);
-        textViews[9] = findViewById(R.id.ten_word_name_textView);
+    private void findTextViewsToArr() {
+        wordNamesTextViews[0] = findViewById(R.id.first_word_name_textView);
+        wordNamesTextViews[1] = findViewById(R.id.second_word_name_textView);
+        wordNamesTextViews[2] = findViewById(R.id.third_word_name_textView);
+        wordNamesTextViews[3] = findViewById(R.id.fourth_word_name_textView);
+        wordNamesTextViews[4] = findViewById(R.id.fifth_word_name_textView);
+        wordNamesTextViews[5] = findViewById(R.id.sixth_word_name_textView);
+        wordNamesTextViews[6] = findViewById(R.id.seventh_word_name_textView);
+        wordNamesTextViews[7] = findViewById(R.id.eight_word_name_textView);
+        wordNamesTextViews[8] = findViewById(R.id.nine_word_name_textView);
+        wordNamesTextViews[9] = findViewById(R.id.ten_word_name_textView);
     }
 
 }
--- app/src/main/java/com/drogunova/neuroapps/neuropsy/TenWordsPresener.java
+++ app/src/main/java/com/drogunova/neuroapps/neuropsy/TenWordsPresener.java
@@ -1,6 +1,10 @@
 package com.drogunova.neuroapps.neuropsy;
 
 import android.content.Context;
+import android.util.Log;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,6 +12,7 @@ import java.util.List;
 
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.schedulers.Schedulers;
 import moxy.InjectViewState;
 import moxy.MvpPresenter;
@@ -16,17 +21,36 @@ import moxy.MvpPresenter;
 public class TenWordsPresener extends MvpPresenter<TenWordsView> {
     private static final String TAG = "TenWordsPresenter";
 
-    List<String> wordsNamesStringsList;
-
-
-    public List <String> takeWords(Context context) {
-        wordsNamesStringsList = new ArrayList<>();
-        wordsNamesStringsList = Arrays.asList(context.getResources().getStringArray(R.array.tenWords_firstWordSet_array));
+    public List<String> getWordsNamesStringsList() {
         return wordsNamesStringsList;
     }
 
-    public Observable<String> getObs(List wordsNamesStringsList) {
-        return (Observable<String>) Observable.fromIterable(wordsNamesStringsList).subscribeOn(Schedulers.io());
+    List<String> wordsNamesStringsList;
+
+    public void setWordsNamesStringsList(List<String> wordsNamesStringsList) {
+        this.wordsNamesStringsList = wordsNamesStringsList;
+    }
+
+
+    public Observable<String> getObs() {
+
+        Observable <String> observable = Observable.fromIterable(wordsNamesStringsList);
+
+        observable.subscribeOn(Schedulers.io());
+//        Observable<String> observable = Observable.create((ObservableOnSubscribe<String>) emitter -> {
+//            Log.d(TAG, "Поток: " + Thread.currentThread().getName());
+//            for (int i = 0; i < wordsNamesStringsList.size(); i++) {
+//                String s = wordsNamesStringsList.get(i);
+//                emitter.onNext(s);
+//            }
+//            emitter.onComplete();
+//        }).subscribeOn(Schedulers.io());
+
+        return observable;
+    }
+
+    public void setNamesToTextViews(){
+        getViewState().setWordsNamesToTextViews();
     }
 
 }
